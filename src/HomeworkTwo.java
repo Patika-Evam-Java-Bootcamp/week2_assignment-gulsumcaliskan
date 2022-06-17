63afc5a9cf5ef9f086179123b29ddedb5eecc06f
Gülsüm ÇALIŞKAN <gulsumfb95@gmail.com>
2022-06-18 00:00:46 +0300

    Update HomeworkTwo.java

--- src/HomeworkTwo.java
+++ src/HomeworkTwo.java
@@ -7,17 +7,22 @@ public class HomeworkTwo {
 		MemberAccountService memberAccountService = new MemberAccountService();
 		
 		//The member is created in member account
-		MemberAccount member = new MemberAccount(1, "Rose", "Gwen", 9500.0, "+53214569878", "1ro");
+		MemberAccount member = new MemberAccount(1, "Rose", "Gwen", 9500.0, "+53214569878", "1ro", "1RoGw");
 		
 		//create method
 //		MemberAccountService memberAccountServ1 = MemberAccountService.create(2, "Mary", "Stefan", 8500.0, "+51214569899", "2ma");
 		
+		Bill bill = new Bill();
+		System.out.println("*****************");
 		
+	
 		
 		try {
-		    BillService billService = new BillService();
-		    Response resp = billService.payTheBill(0, "1ro", 500.0, 9500.0);		
+		    BillService billService = new Client();
+//		    Response resp = billService.payTheBill(0, "1ro", 500.0, 9500.0);		
 //		    Response resp = billService.payTheBill(0, "1ro", 10500.0, 9500.0);
+		    Response resp = new Response();
+		    resp = billService.payTheBill(bill.getBillType(), member.getMemberCode(), bill.getAmount(), member.getBalance(), bill.getProcessDate());
 		    
 		    if(resp.getCode().equals("0")) {
 			    System.out.println("Successfull Process. The time of the created was : " + resp.getCode() + " ----> " + resp.getDate());
